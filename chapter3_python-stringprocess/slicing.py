ab1e323205ba022fc87cf07329bf6a9b494b292c
ksm0207 <ksm03071@naver.com>
2020-09-02 09:29:59 +0900

    # Python_study Part3

--- chapter3_python-stringprocess/slicing.py
+++ chapter3_python-stringprocess/slicing.py
@@ -8,7 +8,7 @@ print("연:", jumin[0:2])  # 0부터 2직전까지 (index : 0,1)
 print("월:", jumin[2:4])
 print("일", jumin[4:6])
 
-print("생년월일:", jumin[0:6])
+print("생년월일:", jumin[0:5])
 # print("생년월일:", jumin[:6]) # 처음부터 6직전까지 가져옴
 
 print("뒤 7자리:", jumin[7:14])
